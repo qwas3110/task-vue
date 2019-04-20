ce119ecc0b09d5dfd954125fedb463f166a47c1a
Laso <laso@149.143.41.120.broad.xm.fj.dynamic.163data.com.cn>
2019-04-20 11:02:32 +0800

    对接浏览器本地存储

---
+++ js/app.js
@@ -0,0 +1,83 @@
+;(function () {
+    'use strict';
+
+    function copy(obj) {
+        return Object.assign({}, obj);
+    }
+
+
+    new Vue({
+        el: '#main',
+        data: {
+            list: [],
+            current: {}
+        },
+
+        mounted: function() {
+            this.list = ms.get('list') || this.list;
+        },
+
+        methods: {
+            // 添加或更新
+            merge() {
+                var is_update,id;
+                is_update = id = this.current.id;
+                if (is_update) {
+                    var index =  this.find_index(id);
+
+                    Vue.set(this.list, index,  copy(this.current));
+
+                } else {
+                    var title = this.current.title;
+
+                    if (!title && title !== 0) return;
+
+                    var todo = copy( this.current);
+                    todo.id  = this.next_id();
+
+                    this.list.push(todo);
+                }
+
+                ms.set('list', this.list);
+                this.reset_current();
+
+            },
+            remove(id) {
+                var index = this.find_index(id);
+                this.list.splice(index,1);
+
+                ms.set('list', this.list);
+            },
+
+            next_id() {
+                return this.list.length + 1;
+            },
+            set_current(todo) {
+                this.current = copy(todo);
+            },
+            reset_current() {
+                this.set_current({});
+            },
+
+            find_index(id) {
+                return this.list.findIndex(function (item) {
+                    return item.id == id;
+                })
+            }
+        },
+
+        watch: {
+            list: {
+                deep: true,
+                handler: function (n,o ) {
+                    if (n) {
+                        ms.set('list', n);
+                    } else {
+                        ms.set('list', []);
+                    }
+                }
+            }
+        }
+    })
+
+})();
